7171066c389255669c5c8921031c998004fa3707
josevargas1229 <20221015@uthh.edu.mx>
2025-12-13 19:51:58 -0600

    Corregir error tipográfico en la asociación de roles

--- src/database/associations.js
+++ src/database/associations.js
@@ -4,15 +4,17 @@ const Cargo = require("../modules/cargo/models/cargo.model");
 const UserRole = require("../modules/roles/models/userRole.model");
 
 User.belongsToMany(Role, { through: UserRole, foreignKey: 'user_id', otherKey: 'role_id', as: 'roles' });
-Role.belongsToMany(User, { through: UserRole, foreignKey: 'role_id', optherKey: 'user_id', as: 'users'});
+Role.belongsToMany(User, { through: UserRole, foreignKey: 'role_id', otherKey: 'user_id', as: 'users' });
 
 User.belongsTo(Cargo, { foreignKey: 'cargo_id', as: 'cargo' });
 Cargo.hasMany(User, { foreignKey: 'cargo_id', as: 'users' });
 
 
-User.belongsTo(User, {as: 'creator', foreignKey: 'created_by'});
-User.belongsTo(User, {as: 'editor', foreignKey: 'updated_by'});
+User.belongsTo(User, { as: 'creator', foreignKey: 'created_by' });
+User.belongsTo(User, { as: 'editor', foreignKey: 'updated_by' });
 
+User.hasMany(User, { as: 'createdUsers', foreignKey: 'created_by' });
+User.hasMany(User, { as: 'updatedUsers', foreignKey: 'updated_by' });
 
 module.exports = {
     User,
