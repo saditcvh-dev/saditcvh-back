12907a491162c232b29a2c576714d70ce121dd18
saditcvh-dev <saditcvh@gmail.com>
2026-03-06 19:19:11 -0600

    Update associations.js

--- src/database/associations.js
+++ src/database/associations.js
@@ -7,57 +7,82 @@ const RolePermission = require("../modules/roles/models/rolePermission.model");
 const Municipio = require("../modules/municipios/models/municipio.model");
 const UserMunicipalityPermission = require("../modules/users/models/userMunicipalityPermission.model");
 const AuditLog = require("../modules/audit/models/auditLog.model");
+const Documento = require("../modules/explorer/models/documento.model");
+const ArchivoDigital = require("../modules/explorer/models/archivo-digital.model");
+const Autorizacion = require("../modules/explorer/models/autorizacion.model");
 
-User.belongsToMany(Role, { through: UserRole, foreignKey: 'user_id', otherKey: 'role_id', as: 'roles' });
-Role.belongsToMany(User, { through: UserRole, foreignKey: 'role_id', otherKey: 'user_id', as: 'users' });
+User.belongsToMany(Role, {
+  through: UserRole,
+  foreignKey: "user_id",
+  otherKey: "role_id",
+  as: "roles",
+});
+Role.belongsToMany(User, {
+  through: UserRole,
+  foreignKey: "role_id",
+  otherKey: "user_id",
+  as: "users",
+});
 
-User.belongsTo(Cargo, { foreignKey: 'cargo_id', as: 'cargo' });
-Cargo.hasMany(User, { foreignKey: 'cargo_id', as: 'users' });
+User.belongsTo(Cargo, { foreignKey: "cargo_id", as: "cargo" });
+Cargo.hasMany(User, { foreignKey: "cargo_id", as: "users" });
 
-User.belongsTo(User, { as: 'creator', foreignKey: 'created_by' });
-User.belongsTo(User, { as: 'editor', foreignKey: 'updated_by' });
+User.belongsTo(User, { as: "creator", foreignKey: "created_by" });
+User.belongsTo(User, { as: "editor", foreignKey: "updated_by" });
 
-User.hasMany(User, { as: 'createdUsers', foreignKey: 'created_by' });
-User.hasMany(User, { as: 'updatedUsers', foreignKey: 'updated_by' });
+User.hasMany(User, { as: "createdUsers", foreignKey: "created_by" });
+User.hasMany(User, { as: "updatedUsers", foreignKey: "updated_by" });
 
 // Un usuario puede tener muchos logs
-User.hasMany(AuditLog, { foreignKey: 'user_id', as: 'audit_logs' });
-AuditLog.belongsTo(User, { foreignKey: 'user_id', as: 'user' });
+User.hasMany(AuditLog, { foreignKey: "user_id", as: "audit_logs" });
+AuditLog.belongsTo(User, { foreignKey: "user_id", as: "user" });
 
 // Roles y sus Permisos Base (La "plantilla" por defecto)
-Role.belongsToMany(Permission, { 
-    through: RolePermission, 
-    foreignKey: 'role_id', 
-    otherKey: 'permission_id', 
-    as: 'base_permissions' 
+Role.belongsToMany(Permission, {
+  through: RolePermission,
+  foreignKey: "role_id",
+  otherKey: "permission_id",
+  as: "base_permissions",
 });
-Permission.belongsToMany(Role, { 
-    through: RolePermission, 
-    foreignKey: 'permission_id', 
-    otherKey: 'role_id' 
+Permission.belongsToMany(Role, {
+  through: RolePermission,
+  foreignKey: "permission_id",
+  otherKey: "role_id",
 });
 
 // Usuarios y su Matriz de Acceso por Municipio
 // Esta relación permite hacer: user.getMunicipalityAccess()
-User.hasMany(UserMunicipalityPermission, { foreignKey: 'user_id', as: 'municipality_access' });
-UserMunicipalityPermission.belongsTo(User, { foreignKey: 'user_id' });
+User.hasMany(UserMunicipalityPermission, {
+  foreignKey: "user_id",
+  as: "municipality_access",
+});
+UserMunicipalityPermission.belongsTo(User, { foreignKey: "user_id" });
 
 // Conexión con Municipios
-UserMunicipalityPermission.belongsTo(Municipio, { foreignKey: 'municipio_id', as: 'municipio' });
-Municipio.hasMany(UserMunicipalityPermission, { foreignKey: 'municipio_id' });
+UserMunicipalityPermission.belongsTo(Municipio, {
+  foreignKey: "municipio_id",
+  as: "municipio",
+});
+Municipio.hasMany(UserMunicipalityPermission, { foreignKey: "municipio_id" });
 
 // Conexión con Permisos
-UserMunicipalityPermission.belongsTo(Permission, { foreignKey: 'permission_id', as: 'permission' });
-Permission.hasMany(UserMunicipalityPermission, { foreignKey: 'permission_id' });
+UserMunicipalityPermission.belongsTo(Permission, {
+  foreignKey: "permission_id",
+  as: "permission",
+});
+Permission.hasMany(UserMunicipalityPermission, { foreignKey: "permission_id" });
 
 module.exports = {
-    User,
-    Role,
-    Cargo,
-    UserRole,
-    Permission,
-    RolePermission,
-    Municipio,
-    UserMunicipalityPermission,
-    AuditLog,
+  User,
+  Role,
+  Cargo,
+  UserRole,
+  Permission,
+  RolePermission,
+  Municipio,
+  UserMunicipalityPermission,
+  AuditLog,
+  Documento,
+  ArchivoDigital,
+  Autorizacion,
 };
